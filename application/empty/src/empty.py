6468d15888099f541015c7d72527a0cbd752a668
redplanet00 <redplanet00@gmail.com>
2022-09-20 21:34:31 +0200

    init controlplane benchmark

---
+++ application/empty/src/empty.py
@@ -0,0 +1,19 @@
+"""\
+This application is used to benchmark the control plane of a resource manager.
+It only consists of a sleep, and print statements to indicate the start and end of the application.
+"""
+
+import os
+import time
+
+SLEEP_TIME = int(os.environ["SLEEP_TIME"])
+
+
+def main():
+    print("Start the application")
+    time.sleep(SLEEP_TIME)
+    print("End the application")
+
+
+if __name__ == "__main__":
+    main()
