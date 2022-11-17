171a0bcbf7224b63199157d92f4a9d93f60123d4
redplanet00 <redplanet00@gmail.com>
2022-11-17 13:59:57 +0100

    bugfix tutorial

--- benchmark/start.py
+++ benchmark/start.py
@@ -326,11 +326,12 @@ def start_endpoint(config, machines):
     return container_names
 
 
-def wait_endpoint_completion(machines, sshs, container_names):
+def wait_endpoint_completion(config, machines, sshs, container_names):
     """Wait for all containers to be finished running the benchmark on endpoints
     OR for all mist containers, which also use docker so this function can be reused
 
     Args:
+        config (dict): Parsed configuration
         machines (list(Machine object)): List of machine objects representing physical machines
         sshs (list(str)): SSH addresses to edge or endpoint VMs
         container_names (list(str)): Names of docker containers launched
@@ -466,12 +467,12 @@ def start(config, machines):
     container_names = start_endpoint(config, machines)
 
     # Wait for benchmark to finish
-    wait_endpoint_completion(machines, config["endpoint_ssh"], container_names)
+    wait_endpoint_completion(config, machines, config["endpoint_ssh"], container_names)
     if config["mode"] == "cloud" or config["mode"] == "edge":
         if config["benchmark"]["resource_manager"] != "mist":
             wait_worker_completion(config, machines)
         else:
-            wait_endpoint_completion(machines, config["edge_ssh"], container_names_mist)
+            wait_endpoint_completion(config, machines, config["edge_ssh"], container_names_mist)
 
     # Now get raw output
     logging.info("Benchmark has been finished, prepare results")
