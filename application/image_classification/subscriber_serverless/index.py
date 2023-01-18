16f1f5d4f1c8ebbbaaf5f1476a4d4e0d05c05106
redplanet00 <redplanet00@gmail.com>
2023-01-19 00:17:29 +0100

    fix linting for openfaas template code

--- application/image_classification/subscriber_serverless/index.py
+++ application/image_classification/subscriber_serverless/index.py
@@ -1,11 +1,18 @@
-# Copyright (c) Alex Ellis 2017. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+"""\
+Copyright (c) Alex Ellis 2017. All rights reserved.
+Licensed under the MIT license. See LICENSE file in the project root for full license information.
+"""
 
 import sys
 from function import handler
 
 
 def get_stdin():
+    """Get input from the trigger and forward the request body to the serverless function
+
+    Returns:
+        str: Input from trigger as string
+    """
     buf = ""
     while True:
         line = sys.stdin.readline()
@@ -18,5 +25,5 @@ def get_stdin():
 if __name__ == "__main__":
     st = get_stdin()
     ret = handler.handle(st)
-    if ret != None:
+    if ret is not None:
         print(ret)
