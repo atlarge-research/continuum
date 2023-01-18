89be067c5e22fdb4eb7e490d9e5467aebfad0865
redplanet00 <redplanet00@gmail.com>
2023-01-18 14:31:30 +0100

    create serverless app

--- application/image_classification/publisher_serverless/docker.sh
+++ application/image_classification/publisher_serverless/docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp -r ../images src/
-docker buildx build --platform linux/amd64,linux/arm64 -t redplanet00/kubeedge-applications:image_classification_publisher --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t redplanet00/kubeedge-applications:image_classification_publisher_serverless --push .
 rm -r src/images
--- application/image_classification/subscriber_serverless/docker.sh
+++ application/image_classification/subscriber_serverless/docker.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cp ../model/* ./src/
-
-# Something with faas-cli build
-#docker buildx build --platform linux/amd64,linux/arm64 -t redplanet00/kubeedge-applications:image_classification_subscriber_serverless --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t redplanet00/kubeedge-applications:image_classification_subscriber_serverless --push .
 rm src/labels.txt src/model.tflite
