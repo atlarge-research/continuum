ca0f7414c28aed3a6681e86134e21c25b775e25a
Matthijs Jansen <34269779+matthijssjansen@users.noreply.github.com>
2023-08-17 18:13:57 +0200

    Update configuration_parser.py

--- configuration_parser/configuration_parser.py
+++ configuration_parser/configuration_parser.py
@@ -349,8 +349,11 @@ def parse_infrastructure(parser, input_config, config):
 
     for s in settings:
         option_check(parser, input_config, config, sec, s[0], s[1], s[2], s[3], s[4])
-    print("basePath: ", config[sec]["base_path"])
-    if config[sec]["base_path"] == "": config[sec]["base_path"] = "~" 
+
+    # TODO This statement shouldn't be needed, but has to be tested first
+    if config[sec]["base_path"] == "":
+        config[sec]["base_path"] = "~" 
+
     config[sec]["base_path"] = os.path.expanduser(config[sec]["base_path"])
     if config[sec]["base_path"][-1] == "/":
         config[sec]["base_path"] = config[sec]["base_path"][:-1]
