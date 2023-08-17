250643616798588d03b59c43b5e091f33fdd3035
Daniel Berzak <d.berzak@student.vu.nl>
2023-08-17 11:42:15 +0200

    removed commented code

--- DSL/basic.ts
+++ DSL/basic.ts
@@ -1,33 +1,57 @@
 import Configuration from "./configuration"
 import { applicationVars } from "./generics"
 
-//TODO: press tab to quickly fill in placeholder values
-const newConfiguration = new Configuration(
-    {
-        infrastructure: {
-            provider: "qemu",
-            nodes: { cloud: 2, edge: 0, endpoint: 3 },
-            cores: { cloud: 2, edge: 2, endpoint: 2 },
-            memory: { cloud: 5, edge: 1, endpoint: 8 },
-            quota: { cloud: 0.5, edge: 0.8, endpoint: 0.4 },
-            middleIP: 244,
-            middleIPBase: 246
 
-        },
-        
-        benchmark: {
-            resourceManager: "kubernetes",
-            application: "image_classification", 
-            applicationVars: applicationVars([
-                ["sleep_time", 60],
-            ])
-        }
+// this is a simple example of a configuration (generated using snippets)
+const configuration = new Configuration({
+    infrastructure: {
+        provider: 'qemu',
+        nodes: { cloud: 0, edge: 1, endpoint: 1 }, 
+        cores: { cloud: 1, edge: 1, endpoint: 11 }, 
+        memory: { cloud: 2, edge: 1, endpoint: 1 }, 
+        quota: { cloud: 0.4, edge: 0.3, endpoint: 0.5 }, 
+    },
+    benchmark: {
+        resourceManager: "kubernetes",
+        application: "empty", 
+        applicationVars: applicationVars([
+            ["sleep_time", 60], 
+            
+        ])
     }
+})
+configuration.validate()
+configuration.output()
 
-)
 
-newConfiguration.validate()
-newConfiguration.output()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //TODO: press tab to quickly fill in placeholder values
