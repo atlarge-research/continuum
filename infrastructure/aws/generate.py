8df654512b1783022aaa8f8a4e8222cf95148248
Ed Reinoso <edgardojesus16@gmail.com>
2023-09-06 20:59:10 +0200

    aws terraform adaptation
    network, elastic ips, instances

--- infrastructure/aws/generate.py
+++ infrastructure/aws/generate.py
@@ -6,18 +6,17 @@ HEADER = """
 terraform {
     required_version = ">= 1.3.6"
     required_providers {
-        google = {
-            source  = "hashicorp/google"
-            version = "4.46.0"
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
         }
     }
 }
 """
 
 PROVIDER = """
-provider "google" {
+provider "aws" {
   credentials = file(%s)
-  project     = %s
   region      = %s
   zone        = %s
 }
@@ -46,74 +45,69 @@ def generate_header(config):
 
 ###################################################################################################
 
+# 1️⃣ Vpc
+# ❓ What is the cidr block for the network
+
+
 MAIN_NETWORK = """
-resource "google_compute_network" "vpc_network" {
+resource "aws_vpc" "vpc_network" {
     name                    = "vpc-network"
-    routing_mode            = "GLOBAL"
-    auto_create_subnetworks = false
+    cidr_block              = "10.0.0.0/16"
 }
 """
 
+# 2️⃣ Subnets
+
 CLOUD_NETWORK = """
-resource "google_compute_subnetwork" "subnetwork_cloud" {
+resource "aws_subnet" "subnetwork_cloud" {
     name          = "subnetwork-cloud"
     ip_cidr_range = "10.0.0.0/24"
-    network       = google_compute_network.vpc_network.id
+    vpc_id        = aws_vpc.vpc_network.id
 }
 """
 
 EDGE_NETWORK = """
-resource "google_compute_subnetwork" "subnetwork_edge" {
+resource "aws_subnet" "subnetwork_edge" {
     name          = "subnetwork-edge"
     ip_cidr_range = "10.0.1.0/24"
-    network       = google_compute_network.vpc_network.id
+    vpc_id        = aws_vpc.vpc_network.id
 }
 """
-
 ENDPOINT_NETWORK = """
-resource "google_compute_subnetwork" "subnetwork_endpoint" {
+resource "aws_subnet" "subnetwork_endpoint" {
     name          = "subnetwork-endpoint"
     ip_cidr_range = "10.0.2.0/24"
-    network       = google_compute_network.vpc_network.id
+    vpc_id        = aws_vpc.vpc_network.id
 }
 """
 
-INGRESS = """
-resource "google_compute_firewall" "allow_all_ingress" {
+# 3️⃣ Security groups
+
+
+SECURITY_GROUP = """
+resource "aws_security_group" "allow_all_ingress" {
     name      = "vpc-network-allow-all-ingress"
-    network   = google_compute_network.vpc_network.name
+    vpc_id    = aws_vpc.vpc_network.id
     direction = "INGRESS"
 
-    allow {
-        protocol = "icmp"
+    ingress {
+        description = "Allow all inbound connection"
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
-    allow {
-        protocol = "tcp"
-        ports    = ["0-65535"]
+    egress {
+        description = "Allow all outbound connection"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
     }
-
-    source_ranges = ["0.0.0.0/0"]
+    
 }
 """
-
-EGRESS = """
-resource "google_compute_firewall" "allow_all_egress" {
-    name      = "vpc-network-allow-all-egress"
-    network   = google_compute_network.vpc_network.name
-    direction = "EGRESS"
-
-    allow {
-        protocol = "icmp"
-    }
-
-    allow {
-        protocol = "tcp"
-        ports    = ["0-65535"]
-    }
-}
-"""
-
 
 def generate_network(config):
     """Write the Terraform config network
@@ -133,125 +127,127 @@ def generate_network(config):
         if config["infrastructure"]["endpoint_nodes"] > 0:
             f.write(ENDPOINT_NETWORK)
 
-        f.write(INGRESS)
-        f.write(EGRESS)
+        f.write(SECURITY_GROUP)
 
 
 ###################################################################################################
 
+# 4️⃣ Elastic IP Addresses
+# ❓ Should there be any associations with the instances here?
 CLOUD_IP = """
-resource "google_compute_address" "cloud_static_ip" {
+resource "aws_eip" "cloud_static_ip" {
     name = "cloud${count.index}-static-ip"
     count = %i
 }
 """
 
 EDGE_IP = """
-resource "google_compute_address" "edge_static_ip" {
+resource "aws_eip" "edge_static_ip" {
     name = "edge${count.index}-static-ip"
     count = %i
 }
 """
 
 ENDPOINT_IP = """
-resource "google_compute_address" "endpoint_static_ip" {
+resource "aws_eip" "endpoint_static_ip" {
     name = "endpoint${count.index}-static-ip"
     count = %i
 }
 """
 
-CLOUD = """
-resource "google_compute_instance" "cloud" {
-    name         = "cloud${count.index}"
-    machine_type = %s
-    count        = %i
+# 5️⃣ Instances
+# ❓ Would service accounts be necessary
+# ❓ Do you need another block device attached to the instance
 
-    boot_disk {
-        initialize_params {
-            size  = "30"
-            type  = "pd-standard"
-            image = "ubuntu-os-cloud/ubuntu-2004-lts"
-        }
+AMI = """
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+"""
+
+CLOUD = """
+resource "aws_network_interface" "cloud_interface" {
+    count       = %i
+    subnet_id   = aws_subnet.subnetwork_cloud.id
+    private_ips = element(aws_eip.cloud_static_ip.*.address, count.index)
+}
+
+resource "aws_instance" "cloud" {
+    name          = "cloud${count.index}"
+    instance_type = %s
+    ami           = data.aws_ami.ubuntu.id
+    count         = %i
+
+    root_block_device {
+        volume_size = 30,
+        volume_type = gp3
     }
 
     network_interface {
-        network    = google_compute_network.vpc_network.name
-        subnetwork = google_compute_subnetwork.subnetwork_cloud.name
-        access_config {
-            nat_ip = element(google_compute_address.cloud_static_ip.*.address, count.index)
-        }
-    }
-
-    service_account {
-        scopes = ["cloud-platform"]
-    }
-
-    metadata = {
-        ssh-keys = "cloud${count.index}:${file("%s")}"
+        network_interface_id = aws_network_interface.cloud_interface.id
+        device_index         = 0
     }
 }
 """
 
 EDGE = """
-resource "google_compute_instance" "edge" {
-    name         = "edge${count.index}"
-    machine_type = %s
-    count        = %i
+resource "aws_network_interface" "edge_interface" {
+    count       = %i
+    subnet_id   = aws_subnet.subnetwork_edge.id
+    private_ips = element(aws_eip.edge_static_ip.*.address, count.index)
+}
 
-    boot_disk {
-        initialize_params {
-            size  = "30"
-            type  = "pd-standard"
-            image = "ubuntu-os-cloud/ubuntu-2004-lts"
-        }
+resource "aws_instance" "edge" {
+    name          = "edge${count.index}"
+    instance_type = %s
+    ami           = data.aws_ami.ubuntu.id
+    count         = %i
+
+    root_block_device {
+        volume_size = 30,
+        volume_type = gp3
     }
 
     network_interface {
-        network    = google_compute_network.vpc_network.name
-        subnetwork = google_compute_subnetwork.subnetwork_edge.name
-        access_config {
-            nat_ip = element(google_compute_address.edge_static_ip.*.address, count.index)
-        }
-    }
-
-    service_account {
-        scopes = ["cloud-platform"]
-    }
-
-    metadata = {
-        ssh-keys = "edge${count.index}:${file("%s")}"
+        network_interface_id = aws_network_interface.edge_interface.id
+        device_index         = 0
     }
 }
 """
 
 ENDPOINT = """
-resource "google_compute_instance" "endpoint" {
-    name         = "endpoint${count.index}"
-    machine_type = %s
-    count        = %i
+resource "aws_network_interface" "endpoint_interface" {
+    count       = %i
+    subnet_id   = aws_subnet.subnetwork_endpoint.id
+    private_ips = element(aws_eip.endpoint_static_ip.*.address, count.index)
+}
 
-    boot_disk {
-        initialize_params {
-            size  = "30"
-            type  = "pd-standard"
-            image = "ubuntu-os-cloud/ubuntu-2004-lts"
-        }
+resource "aws_instance" "endpoint" {
+    name          = "endpoint${count.index}"
+    instance_type = %s
+    ami           = data.aws_ami.ubuntu.id
+    count         = %i
+
+    root_block_device {
+        volume_size = 30,
+        volume_type = gp3
     }
 
     network_interface {
-        network    = google_compute_network.vpc_network.name
-        subnetwork = google_compute_subnetwork.subnetwork_endpoint.name
-        access_config {
-            nat_ip = element(google_compute_address.endpoint_static_ip.*.address, count.index)
-        }
-    }
-
-    service_account {
-        scopes = ["cloud-platform"]
-    }
-
-    metadata = {
-        ssh-keys = "endpoint${count.index}:${file("%s")}"
+        network_interface_id = aws_network_interface.endpoint_interface.id
+        device_index         = 0
     }
 }
 """
