49d8c720a3d0541c0f8db870181ea954c4b2c09b
Antonis Sklavos <antonis@sklavos.io>
2023-11-06 22:25:16 +0100

    micro benchmarks

---
+++ run_all_antonis_3b_cpu_micro.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+folder="configuration/antonis_thesis/3_resource_usage/b_cpu_micro/strong_scalability"
+
+for file_path in "$folder"/*.cfg; do
+
+    python3 continuum.py "$file_path"
+
+    file_name=$(basename "$file_path" .cfg)
+
+    mkdir -p "logs/$file_name"
+
+    mv logs/*.pdf "logs/$file_name/"
+    mv logs/*.log "logs/$file_name/"
+    mv logs/*.csv "logs/$file_name/"
+done
