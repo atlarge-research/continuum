fa4f9c52fb0bc8dc1dcc36b55732abb24a3eb897
Antonis Sklavos <antonis@sklavos.io>
2023-11-20 15:03:20 +0100

    cleaned up cfg

--- run_all_antonis_3b_cpu_micro.sh
+++ run_all_antonis_3b_cpu_micro.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-folder="configuration/antonis_thesis/3_resource_usage/b_cpu_micro/strong_scalability"
+# folder="configuration/antonis_thesis/3_resource_usage/b_cpu_micro/strong_scalability"
+
+# for file_path in "$folder"/*.cfg; do
+
+#     echo python3 continuum.py "$file_path"
+#     python3 continuum.py "$file_path"
+
+#     file_name=$(basename "$file_path" .cfg)
+
+#     mkdir -p "logs/$file_name"
+
+#     mv logs/*.pdf "logs/$file_name/"
+#     mv logs/*.log "logs/$file_name/"
+#     mv logs/*.csv "logs/$file_name/"
+# done
+
+folder="configuration/antonis_thesis/3_resource_usage/b_cpu_micro/weak_scalability"
 
 for file_path in "$folder"/*.cfg; do
 
